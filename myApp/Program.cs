c55467376cd15c6ad4ed8d030e05214c7f3223f5
mjtedder <mjtedder@gmail.com>
2019-02-25 18:59:53 -0700

    String Methods
    I have made some strings and practiced using string methods such as
    .Length which returns how many characters a string variable is.
    
    I also used TrimStart and TrimEnd which removes whitespace from before
    and after the characters of a variable.
    
    This is coming from the .NET/C# documentation tutorials.

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -6,10 +6,34 @@ namespace myApp
     {
         static void Main(string[] args)
         {
+            // Setting a string variable
             string aMessage = "Hello World!";
+            // Reassigning the variable
             aMessage = "World!";
+            Console.WriteLine("-----------------------------------------");
             Console.WriteLine("Hello " + aMessage);
+            Console.WriteLine("------------------------------------------");
+
+            // STRING INTERPOLATION
             Console.WriteLine($"The current time is {DateTime.Now}");
+
+            // Assigning string variable favorite bands
+            string bands1 = "Umphrey's McGee";
+            string bands2 = "Tedeschi Trucks";
+            Console.WriteLine($"\nMy favorite bands are {bands1} and {bands2}");
+
+            // Using .length to count the number of letters in a string
+            Console.WriteLine($"\nThe band {bands1} has {bands1.Length} letters");
+            Console.WriteLine("--------------------------------------------------\n");
+            // STRING METHODS
+            string greeting = "                        Good evening!             ";
+            Console.WriteLine($"[{greeting}]");
+
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"\n[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"\n[{trimmedGreeting}]");
         }
     }
 }
