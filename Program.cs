6eb6ff795b8a551710f75feae4fcf48c2f745ca7
mjtedder <mjtedder@gmail.com>
2019-02-25 20:10:37 -0700

    Create new program
    I created a new working environment to demonstrate what C# can do with
    the integer data-type.
    
    Right now it takes in two int variables and the third int variable
    performs the sum of the first two variables.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
