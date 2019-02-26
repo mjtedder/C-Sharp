43b6c0d0ede518ae293f5ea651c4f38dd2f82180
mjtedder <mjtedder@gmail.com>
2019-02-26 14:10:26 -0700

    Reassign int variable
    I reassigned  the int variable to do basic mathematical operations.
    I also moved the integers into a new method and assign the method inside
    of the main method.
    
    I am unsure at this point, but I speculate that C# methods are similar
    to JavaScript functions.

--- numbers-quickstart/Program.cs
+++ numbers-quickstart/Program.cs
@@ -4,12 +4,26 @@ namespace NumbersInCSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void WorkingWithIntegers()
         {
             int a = 18;
             int b = 6;
             int c = a + b;
-            Console.WriteLine(c);
+            Console.WriteLine("ADD");
+            Console.WriteLine(c + "\n");
+            c = a - b;
+            Console.WriteLine("SUBTRACT");
+            Console.WriteLine(c + "\n");
+            c = a * b;
+            Console.WriteLine("MULTIPLY");
+            Console.WriteLine(c + "\n");
+            c = a / b;
+            Console.WriteLine("DIVIDE");
+            Console.WriteLine(c + "\n");
+        }
+        static void Main(string[] args)
+        {
+            WorkingWithIntegers();
         }
     }
 }
