95c5390452db057a9d62e89d65938c78a86ec27d
mjtedder <mjtedder@gmail.com>
2019-02-27 08:32:53 -0700

    Create Models folder
    I added a Movie class to the RazorPagesMovie model which contains
    various fields about a movie that the user will need to input when
    adding movies to the db.
    
    I then created an  entity set RazorPagesMovieContext  which corresonds to a database table.

---
+++ RazorPagesMovie/Models/RazorPagesMovieContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Models
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
